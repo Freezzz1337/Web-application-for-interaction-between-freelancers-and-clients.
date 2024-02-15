6ac5dfd938287e6c27f8520a55290653da967587
Artem Samokysh <samokish777bombila@gmail.com>
2024-02-15 20:47:07 +0200

    create backend and fronted projects

---
+++ backend_graduate_work/src/main/java/backend_graduate_work/BackendGraduateWorkApplication.java
@@ -0,0 +1,13 @@
+package backend_graduate_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGraduateWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGraduateWorkApplication.class, args);
+	}
+
+}
---
+++ backend_graduate_work/src/test/java/backend_graduate_work/BackendGraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_graduate_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGraduateWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
